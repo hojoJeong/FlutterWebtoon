26eba9ae61cd1192813c533788ee55f2c78ec92f
hojo0423 <hojo0423@naver.com>
2023-04-26 13:49:46 +0900

    complete

---
+++ webtoon/lib/models/webtoon_detail_model.dart
@@ -0,0 +1,9 @@
+class WebtoonDeatilModel {
+  final String title, about, genre, age;
+
+  WebtoonDeatilModel.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        about = json['about'],
+        genre = json['genre'],
+        age = json['age'];
+}
---
+++ webtoon/lib/screens/home_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:webtoon/models/webtoon_model.dart';
+import 'package:webtoon/services/api_services.dart';
+import 'package:webtoon/widgets/webtoos_widget.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 2,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: const Text(
+          'today toon',
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.w400),
+        ),
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Expanded(child: makeList(snapshot))
+              ],
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      itemBuilder: (context, index) {
+        print(index);
+        var webtoon = snapshot.data![index];
+        return Webtoon(
+            thumb: webtoon.thumb, title: webtoon.title, id: webtoon.id);
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40,
+      ),
+    );
+  }
+}
